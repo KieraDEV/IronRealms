00641d6d461fef0eb03be005bd8c5bbce5a69741
Bonjo <52904880+KieraDEV@users.noreply.github.com>
2021-03-11 20:44:00 -0600

    Create funct.js

---
+++ funct.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  var copyText = document.getElementById("myInput");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  document.execCommand("copy");
+
+  alert("Copied: " + copyText.value);
+}
